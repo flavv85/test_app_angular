736c38be1bfad8b9f8b6772e39b1d34a6f062869
Flavius_Gorgan <gorganflavius@gmail.com>
2021-02-22 11:02:38 +0200

    added dashboard components

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+// securizam ruta si verificam sa afisa dashboard daca avem user logat, altfel redirect catre login
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  userEmail: string;
+  isLogged: boolean;
+  loading: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public afAuth: AngularFireAuth, public router: Router) {
+    this.loading = true;
+    afAuth.onAuthStateChanged((user) => {
+      this.loading = false;
+      this.userEmail = user.email;
+    });
+  }
+  
+  logout() {
+    this.afAuth.signOut();
   }
 
+  ngOnInit(): void {}
 }
+
+
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { ApiService } from '../../api.service';
-import { AngularFireAuth} from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { FirebaseApp } from '@angular/fire';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,13 +16,16 @@ export class LoginComponent implements OnInit {
   loginFormGroup: FormGroup;
   email: string = 'email';
   password: string = 'password';
+  message:string;
+  subscription: Subscription;
 
   isValid: boolean = false;
 
   constructor(
     private api: ApiService,
     public afAuth: AngularFireAuth,
-    ) {}
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginFormGroup = new FormGroup({
@@ -39,12 +43,28 @@ export class LoginComponent implements OnInit {
       .subscribe((res) => console.log(res));
     // console.log(this.loginFormGroup.value)
   }
-  loginWithGoogle(){
-    this.afAuth.signInWithPopup(new firebase.default.auth.GoogleAuthProvider());
+  async loginWithGoogle() {
+    try {
+      await this.afAuth.signInWithPopup(
+        new firebase.default.auth.GoogleAuthProvider()
+      );
+      let user = firebase.default.auth().currentUser;
+      if (user) {
+        this.router.navigate(['/dashboard'])
+        console.log(user.email);
+      } else {
+        console.log('no user');
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   onChange() {
     console.log(this.loginFormGroup.valid);
     this.isValid = this.loginFormGroup.valid;
+  }
+  logout() {
+    this.afAuth.signOut();
   }
 }
